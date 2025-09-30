a7c9cf13ce04e94962255fbcd87dee46afd97369
Marsssss42 <omarrafly25@gmail.com>
2025-09-30 16:46:14 +0700

    Update App.jsx

--- react-chess-front/src/App.jsx
+++ react-chess-front/src/App.jsx
@@ -1,6 +1,9 @@
+<<<<<<< Updated upstream
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+=======
+>>>>>>> Stashed changes
 import './App.css'
 
 function App() {
